0ddff562b392d00658ccf739c109f5a5348b8bd2
Denizey Platform <denizey.platform@outlook.com>
2025-04-28 23:33:39 +0300

    clean architecture setup

---
+++ Talabat.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Talabat.Infrastructure.Models;
+
+namespace Talabat.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser , IdentityRole ,string >
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+       : base(options)
+        {
+        }
+    }
+}
